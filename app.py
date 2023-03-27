b9871b91b58f676a5f59232ab6a81bc20fba8bb2
Jude Sajith <judesajith.aj@gmail.com>
2023-03-27 14:48:01 -0400

    ssss

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ if zip_file is not None:
     index.save_to_disk('indx.json')
     st.success('index created successfully')
 
-# try:
-#     index = GPTSimpleVectorIndex.load_from_disk('indx.json')
-# except FileNotFoundError:
-#     # Loading from a directory
     
     sources = os.listdir("scorm/scormcontent")
     st.write(sources)
@@ -60,57 +56,71 @@ with st.expander("Course Description small"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate small"):
         cdesquery =  index.query("Generate a one-sentence summary , of the course Description.")
-        st.code(cdesquery)
+        st.code(st.session_state['cdesquery'])
 
 with st.expander("Course Description medium"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Medium"):
         cdesquerym =  index.query("Generate a three-sentence summary , of the course Description.")
-        st.code(cdesquerym)
+        st.code(st.session_state['cdesquerym'])
         
 with st.expander("Course Description Large"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Large"):
         cdesqueryl =  index.query("Generate a five-sentence summary , of the course Description.")
-        st.code(cdesqueryl)
+        if 'cdesqueryl' not in st.session_state:
+            st.session_state['cdesqueryl'] = cdesqueryl
+        st.code(st.session_state['cdesqueryl'])
         
 with st.expander("Learning Objectives"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Objectives"):
         lo =  index.query("Generate Learning Objectives of the course material which Should be a list of 3-5 clear and measurable objectives that describe the information, skills, behaviors, or perspectives that the participants will acquire after reading and completing the course material. The learning objectives should clearly identify the outcomes or actions participants can expect to demonstrate as a result of the educational experience. Each objective should start with a verb, and depending on the complexity of the course material, use the language and levels found in Bloom’s taxonomy.")
-        st.code(lo)
+        if 'lo' not in st.session_state:
+            st.session_state['lo'] = lo
+        st.code(st.session_state['lo'])
         
 with st.expander("Marketing Email"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Marketing-email"):
         me =  index.query("Generate a Marketing email for this course, Write 5 clever subject lines (formatted with bullet points) and an email that could be used to generate excitement and interest in the training course. The email should contain the Course Description (Medium) snippet and the Learning Objectives snippet.")
+        if 'me' not in st.session_state:
+            st.session_state['me'] = me
         st.code(me)
 
 with st.expander("Enrolment Email"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Enrollment-email"):
         ee =  index.query("Generate a Enrollment email for this course, Write 5 subject lines (formatted with bullet points) and an email that would be sent to a participant who has signed up for this training course. It should confirm that they are now ready and able to access the course at the following link: http://www.insert_course_link_here.com. The tone of the email should be practical and slightly enthusiastic, and it should describe what the individual will learn in the training course. The email should use the Course Description (Medium) text snippet and include the Learning Objectives snippet")
-        st.code(ee)
+        if 'ee' not in st.session_state:
+            st.session_state['ee'] = ee
+        st.code(st.session_state['ee'])
       
 with st.expander("Completion Email"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Completion-email"):
         cc =  index.query("Generate a Completion email for this course,Write 5 subject lines (formatted with bullet points) and an email that would be sent to a participant who has completed this training course. It should confirm that they have finished the experience and it describe what they learned")
-        st.code(cc)
+        if 'cc' not in st.session_state:
+            st.session_state['cc'] = cc
+        st.code(st.session_state['cc'])
       
 with st.expander("Evaluation Questions (Knowledge Change)"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Evaluation Questions-kc"):
         eq =  index.query("Generate a Evaluation Questions for this course, Write 5-10 quiz questions to evaluate if a participant gained the knowledge and skills outlined in the course material and the Learning Objectives snippet.")
-        st.code(eq)
+        if 'eq' not in st.session_state:
+            st.session_state['eq'] = eq
+        st.code(st.session_state['eq'])
       
 with st.expander("Evaluation Questions (Performance Change)"):
     # st.selectbox("Size",("Medium","Short","Long"))
     if st.button("Generate Evaluation Questions-pc"):
         pc =  index.query("Generate a Evaluation Questions for this course, Write 5-10 quiz questions to evaluate if a participant’s behavior or performance related to the course material may have changed  in their day to day activities (i.e., have they changed how they do their job, as it relates to the course material and the Learning Objectives).")
-        st.code(pc)
+        if 'pc' not in st.session_state:
+            st.session_state['pc'] = pc
+        st.code(st.session_state['pc'])
      
 with st.expander("Copy everything together"):
-    string = "Learning Objectives\n" + lo + "\n\n\n" + "Marketing email\n" + me + "\n\n\n" 
+    string = "Learning Objectives\n" + st.session_state['lo'] + "\n\n\n" + "Marketing email\n" + st.session_state['me'] + "\n\n\n" 
     st.code(string)
 
